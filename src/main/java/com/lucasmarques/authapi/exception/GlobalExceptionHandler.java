7899eb0eadb1c09afe746d6552241a2c93bb67bd
Lucas Marques <lucaamarques2406@gmail.com>
2026-08-20 11:04:18 -0300

    feat: add global exception handling with error response DTO

---
+++ src/main/java/com/lucasmarques/authapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package com.lucasmarques.authapi.exception;
+
+import com.lucasmarques.authapi.dto.ErrorResponse;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrity(MethodArgumentNotValidException exception) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "validation failure",
+                "one or more invalid fields",
+                errors
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrity(DataIntegrityViolationException exception) {
+
+        Map<String, String> errors = new HashMap<>();
+
+       String friendlyMessage = "Unable to complete the registration.";
+
+
+       ErrorResponse errorResponse = new ErrorResponse(
+               LocalDateTime.now(),
+               HttpStatus.CONFLICT.value(),
+               "Registration Conflict",
+               friendlyMessage,
+               errors
+       );
+       return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+}
